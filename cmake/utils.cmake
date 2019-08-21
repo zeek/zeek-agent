d438431108e6cd4b0d3c0cbc5aec9c2c51e981d4
Alessandro Gario <alessandro.gario@gmail.com>
2019-08-21 23:47:48 +0200

    CMake: Convert the actor-framework and broker libraries

--- cmake/utils.cmake
+++ cmake/utils.cmake
@@ -8,56 +8,24 @@
 
 cmake_minimum_required(VERSION 3.10)
 
-function(importBroker)
-  add_library(thirdparty_broker STATIC IMPORTED GLOBAL)
-  set_target_properties(thirdparty_broker PROPERTIES IMPORTED_LOCATION
-    /usr/local/osquery/lib/libbroker.a
+function(generateZeekSettingsTarget)
+  add_library(zeek_common_settings INTERFACE)
+  target_compile_options(zeek_common_settings INTERFACE
+    -Wall
+    -Wextra
+    -Werror
+    -Weverything
+    -pedantic
   )
 
-  target_include_directories(thirdparty_broker SYSTEM INTERFACE
-    /usr/local/osquery/include
-  )
-endfunction()
+  add_library(zeek_c_settings INTERFACE)
+  target_link_libraries(zeek_c_settings INTERFACE zeek_common_settings)
 
-function(importCaf)
-  add_library(thirdparty_caf INTERFACE)
+  add_library(zeek_cxx_settings INTERFACE)
+  target_link_libraries(zeek_cxx_settings INTERFACE zeek_common_settings)
 
-  set(library_name_list
-    caf_core
-    caf_io
-    caf_openssl
-  )
-
-  foreach(library_name ${library_name_list})
-    set(full_library_name "thirdparty_${library_name}")
-
-    add_library("${full_library_name}" STATIC IMPORTED GLOBAL)
-    set_target_properties("${full_library_name}" PROPERTIES IMPORTED_LOCATION
-      "/usr/local/osquery/lib/lib${library_name}_static.a"
-    )
-
-    target_include_directories("${full_library_name}" SYSTEM INTERFACE
-      /usr/local/osquery/include
-    )
-
-    target_link_libraries(thirdparty_caf INTERFACE
-      "${full_library_name}"
-    )
-  endforeach()
-
-  importOpenSSL()
-  target_link_libraries(thirdparty_caf_openssl INTERFACE
-    thirdparty_openssl
-  )
-endfunction()
-
-function(importOpenSSL)
-  add_library(thirdparty_openssl STATIC IMPORTED GLOBAL)
-  set_target_properties(thirdparty_openssl PROPERTIES IMPORTED_LOCATION
-    /usr/local/osquery/lib/libssl.a
-  )
-
-  target_include_directories(thirdparty_openssl SYSTEM INTERFACE
-    /usr/local/osquery/include
-  )
+  # The CAF and Broker versions we have will not compile correctly
+  # with C++14 and above
+  target_compile_features(zeek_c_settings INTERFACE c_std_11)
+  target_compile_features(zeek_cxx_settings INTERFACE cxx_std_14)
 endfunction()
