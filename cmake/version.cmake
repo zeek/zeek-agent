b9c7accf025d2e36964b345ccbf1eb172f43ac4f
Alessandro Gario <alessandro.gario@gmail.com>
2020-01-06 22:30:53 +0100

    CMake: Update version to 1.0.1

--- cmake/version.cmake
+++ cmake/version.cmake
@@ -3,4 +3,4 @@ cmake_minimum_required(VERSION 3.14)
 # This field is referenced
 # 1. By the main project: CMakeLists.txt
 # 2. By the package builder: packaging/CMakeLists.txt
-set(ZEEK_AGENT_VERSION "1.0.0")
+set(ZEEK_AGENT_VERSION "1.0.1")
