1347f20ee5fb32053248eb7372b978097f6e97a9
Alessandro Gario <5714290+alessandrogario@users.noreply.github.com>
2020-01-28 18:03:51 +0100

    scripts: Update the release build script (#13)

--- scripts/build_release.sh
+++ scripts/build_release.sh
@@ -1,43 +1,26 @@
 #!/usr/bin/env bash
 
-export CMAKE_VERSION="3.15.5"
+export CMAKE_VERSION="3.16.3"
+export OSQUERY_VERSION="4.1.1"
+export OSQUERY_TOOLCHAIN_VERSION="1.0.0"
 
 main() {
-  if [[ $# -lt 1  || $# -gt 2 ]] ; then
+  if [[ $# -ne 2 ]] ; then
     printUsage
     return 1
   fi
 
-  if [[ "$1" == "--help" ]] ; then
+  if [[ "$1" != "--zeek-version" ]] ; then
+    printf "Invalid parameter specified: ${1}\n"
     printUsage
     return 1
   fi
 
-  local build_type="$1"
-  if [[ "${build_type}" == "--system" ]] ; then
-    printf "Building with the system compiler\n"
-
-  elif [[ "${build_type}" == "--portable" ]] ; then
-    printf "Building a portable version with the osquery toolchain\n"
-
-  elif [[ "${build_type}" == "--portable-osquery" ]] ; then
-    printf "Building a portable version with osquery support enabled\n"
-
-  else
+  local zeek_version="$2"
+  if [[ "${zeek_version}" != "3.0" && "${zeek_version}" != "3.1" ]] ; then
+    printf "Invalid zeek version specified: ${zeek_version}\n"
     printUsage
-    printf "Invalid build type specified: ${build_type}\n"
     return 1
-  fi
-
-  local only_build_package=0
-  if [[ $# -eq 2 ]] ; then
-    if [[ "$2" == "--package-only" ]] ; then
-      only_build_package=1
-    else
-      printUsage
-      printf "Invalid flag specified: $2\n"
-      return 1
-    fi
   fi
 
   if [ ! -d "build" ] ; then
@@ -47,11 +30,11 @@ main() {
       mkdir "build"
   fi
 
-  if [ ! -d "package_build" ] ; then
+  if [ ! -d "package" ] ; then
     executeCommand \
-      "Creating the package_build folder" \
+      "Creating the package folder" \
       . \
-      mkdir "package_build"
+      mkdir "package"
   fi
 
   if [ ! -d "build/ccache" ] ; then
@@ -83,7 +66,7 @@ main() {
   executeCommand \
     "Installing system dependencies" \
     . \
-    sudo apt-get install clang clang-tidy-8 cppcheck ccache curl libssl-dev flex bison rpm doxygen -y
+    sudo apt-get install cppcheck ccache curl flex bison rpm doxygen ninja-build graphviz -y
 
   executeCommand \
     "Synchronizing the submodules" \
@@ -120,50 +103,45 @@ main() {
     printf " > Using cached CMake install directory: ${cmake_install_path}\n"
   fi
 
-  local osquery_toolchain_version="1.0.0"
-  local osquery_toolchain_url="https://github.com/osquery/osquery-toolchain/releases/download/${osquery_toolchain_version}/osquery-toolchain-${osquery_toolchain_version}.tar.xz"
-  local osquery_toolchain_tarball_path="$(realpath build/downloads)/osquery-toolchain-${osquery_toolchain_version}.tar.xz"
+  local osquery_toolchain_url="https://github.com/osquery/osquery-toolchain/releases/download/${OSQUERY_TOOLCHAIN_VERSION}/osquery-toolchain-${OSQUERY_TOOLCHAIN_VERSION}.tar.xz"
+  local osquery_toolchain_tarball_path="$(realpath build/downloads)/osquery-toolchain-${OSQUERY_TOOLCHAIN_VERSION}.tar.xz"
   local osquery_toolchain_install_path="$(realpath build)/osquery-toolchain"
 
-  if [[ "${build_type}" == "--portable" || "${build_type}" == "--portable-osquery" ]] ; then
-    if [ ! -d "${osquery_toolchain_install_path}" ] ; then
-      if [ ! -f "${osquery_toolchain_tarball_path}" ] ; then
-        executeCommand \
-          "Downloading the osquery toolchain" \
-          . \
-          curl -L "${osquery_toolchain_url}" -o "${osquery_toolchain_tarball_path}"
-
-      else
-        printf " > Using cached osquery toolchain tarball: ${osquery_toolchain_tarball_path}\n"
-      fi
-
+  if [ ! -d "${osquery_toolchain_install_path}" ] ; then
+    if [ ! -f "${osquery_toolchain_tarball_path}" ] ; then
       executeCommand \
-        "Extracting the osquery toolchain" \
-        "build" \
-        tar xf "${osquery_toolchain_tarball_path}"
+        "Downloading the osquery toolchain" \
+        . \
+        curl -L "${osquery_toolchain_url}" -o "${osquery_toolchain_tarball_path}"
 
     else
-      printf " > Using cached osquery toolchain directory: ${osquery_toolchain_install_path}\n"
+      printf " > Using cached osquery toolchain tarball: ${osquery_toolchain_tarball_path}\n"
     fi
+
+    executeCommand \
+      "Extracting the osquery toolchain" \
+      "build" \
+      tar xf "${osquery_toolchain_tarball_path}"
+
+  else
+    printf " > Using cached osquery toolchain directory: ${osquery_toolchain_install_path}\n"
   fi
 
-  if [[ "${build_type}" == "--portable-osquery" ]] ; then
-    if [ -d "osquery" ] ; then
-      printf " i Using existing osquery folder\n"
+  if [ -d "osquery" ] ; then
+    printf " i Using existing osquery folder\n"
 
-    else
-      executeCommand \
-        "Downloading the osquery source code" \
-        . \
-        git clone --branch "4.1.1" https://github.com/osquery/osquery
-    fi
+  else
+    executeCommand \
+      "Downloading the osquery source code for version ${OSQUERY_VERSION}" \
+      . \
+      git clone --branch "${OSQUERY_VERSION}" https://github.com/osquery/osquery
+  fi
 
-    if [ ! -d "$(realpath osquery)/external/extension_zeek-agent" ] ; then
-      executeCommand \
-        "Linking the Zeek agent source folder to osquery/external" \
-        . \
-        ln -s "$(pwd)" "$(realpath osquery)/external/extension_zeek-agent"
-    fi
+  if [ ! -d "$(realpath osquery)/external/extension_zeek-agent" ] ; then
+    executeCommand \
+      "Linking the Zeek agent source folder to osquery/external" \
+      . \
+      ln -s "$(pwd)" "$(realpath osquery)/external/extension_zeek-agent"
   fi
 
   local ccache_folder="$(realpath build/ccache)"
@@ -171,83 +149,46 @@ main() {
   local install_destination="$(realpath build/install)"
 
   export CCACHE_DIR="${ccache_folder}"
-  export PATH="${osquery_toolchain_install_path}:${cmake_install_path}/bin:${PATH}"
+  export PATH="${cmake_install_path}/bin:${PATH}"
 
-  if [[ ${only_build_package} -eq 1 ]] ; then
-    local test_switch="-DZEEK_AGENT_ENABLE_TESTS:BOOL=false"
-    local documentation_switch="-DZEEK_AGENT_ENABLE_DOCUMENTATION:BOOL=false"
-    local sanitizers_switch="-DZEEK_AGENT_ENABLE_SANITIZERS:BOOL=false"
-  else
-    local test_switch="-DZEEK_AGENT_ENABLE_TESTS:BOOL=true"
-    local documentation_switch="-DZEEK_AGENT_ENABLE_DOCUMENTATION:BOOL=true"
-    local sanitizers_switch="-DZEEK_AGENT_ENABLE_SANITIZERS:BOOL=true"
-  fi
-
-  if [[ "${build_type}" == "--system" ]] ; then
-    executeCommand \
-      "Configuring the project (system build)" \
-      "build" \
-      cmake -DCMAKE_C_COMPILER:STRING=clang -DCMAKE_CXX_COMPILER:STRING=clang++ -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo -DCMAKE_INSTALL_PREFIX:PATH="${install_prefix}" "${documentation_switch}" -DZEEK_AGENT_ENABLE_INSTALL:BOOL=true "${sanitizers_switch}" "${test_switch}" -DZEEK_AGENT_ENABLE_INSTALL:BOOL=true ..
-
-  elif [[ "${build_type}" == "--portable" ]] ; then
-    executeCommand \
-      "Configuring the project (portable)" \
-      "build" \
-      cmake -DZEEK_AGENT_TOOLCHAIN_PATH="${osquery_toolchain_install_path}" -DCMAKE_INSTALL_PREFIX:PATH="${install_prefix}" -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo "${documentation_switch}" -DZEEK_AGENT_ENABLE_INSTALL:BOOL=true "${sanitizers_switch}" "${test_switch}" -DZEEK_AGENT_ENABLE_INSTALL:BOOL=true ..
-
-    executeCommand \
-      "Building OpenSSL" \
-      "build" \
-      cmake --build . --target "thirdparty_openssl_builder"
-
-  else
-    executeCommand \
-      "Configuring the project (portable-osquery)" \
-      "build" \
-      cmake -DOSQUERY_TOOLCHAIN_SYSROOT="${osquery_toolchain_install_path}" -DCMAKE_INSTALL_PREFIX:PATH="${install_prefix}" -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo "${documentation_switch}" -DZEEK_AGENT_ENABLE_INSTALL:BOOL=true "${test_switch}" "${sanitizers_switch}" ../osquery
-  fi
-
-  local job_count="$(($(nproc)+1))"
-  printf " i Building with ${job_count} jobs\n"
+  executeCommand \
+    "Configuring the project (portable-osquery)" \
+    "build" \
+    cmake -DOSQUERY_TOOLCHAIN_SYSROOT="${osquery_toolchain_install_path}" -DCMAKE_INSTALL_PREFIX:PATH="${install_prefix}" -DCMAKE_BUILD_TYPE:STRING=RelWithDebInfo -DZEEK_AGENT_ZEEK_COMPATIBILITY:STRING="${zeek_version}" -DZEEK_AGENT_ENABLE_DOCUMENTATION:BOOL=true -DZEEK_AGENT_ENABLE_INSTALL:BOOL=true -DZEEK_AGENT_ENABLE_TESTS:BOOL=true -DZEEK_AGENT_ENABLE_SANITIZERS:BOOL=false -G Ninja ../osquery
 
   executeCommand \
     "Building the project" \
     "build" \
-    cmake --build . -j "${job_count}"
+    cmake --build . -- -v
 
-  if [[ ${only_build_package} -eq 0 ]] ; then
-    executeCommand \
-      "Generating the Doxygen documentation" \
-      "build" \
-      cmake --build . --target "doxygen"
-  fi
+  executeCommand \
+    "Running the tests" \
+    "build" \
+    cmake --build . --target zeek_agent_tests -- -v
+
+  executeCommand \
+    "Generating the Doxygen documentation" \
+    "build" \
+    cmake --build . --target doxygen -- -v
+
+  export DESTDIR="${install_destination}"
 
   executeCommand \
     "Running the install target" \
     "build" \
-    cmake --build . --target install -- DESTDIR="${install_destination}"
-
-  if [[ "${build_type}" == "--portable-osquery" ]] ; then
-    export ASAN_OPTIONS=detect_container_overflow=0
-  fi
-
-  if [[ ${only_build_package} -eq 0 ]] ; then
-    executeCommand \
-      "Running the tests" \
-      "build" \
-      cmake --build . --target "zeek_agent_tests"
-  fi
+    cmake --build . --target install -- -v 
 
   executeCommand \
     "Configuring the packaging project" \
     "package_build" \
-    cmake -DZEEK_AGENT_INSTALL_PATH:PATH="${install_destination}" ../packaging
+    cmake -G Ninja -DZEEK_AGENT_ZEEK_COMPATIBILITY:STRING="${zeek_version}" -DZEEK_AGENT_INSTALL_PATH:PATH="${install_destination}" -DCMAKE_INSTALL_PREFIX:PATH="${install_prefix}" ../packaging
 
   executeCommand \
     "Generating packages" \
     "package_build" \
-    cmake --build . --target "package"
+    cmake --build . --target package -- -v
 
+  printf "\n\nDone! Packages are located in the 'package_build' folder\n"
   return 0
 }
 
@@ -265,7 +206,7 @@ executeCommand() {
 }
 
 printUsage() {
-  printf "Usage:\n\tbuild_release.sh <--system | --portable | --portable-osquery> [--package-only]\n\n"
+  printf "Usage:\n\tbuild_release.sh --zeek-version [3.0|3.1]\n\n"
 }
 
 main $@
