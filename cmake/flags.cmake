389143a79ebfc5fade267a067ccb127e5f7d6512
Alessandro Gario <5714290+alessandrogario@users.noreply.github.com>
2020-03-31 21:03:29 +0200

    CMake: Correctly detect gcc in flags.cmake (#47)

--- cmake/flags.cmake
+++ cmake/flags.cmake
@@ -1,6 +1,8 @@
 cmake_minimum_required(VERSION 3.16.3)
 
-if("${CMAKE_C_COMPILER_ID}" STREQUAL "Clang" OR "${CMAKE_C_COMPILER_ID}" STREQUAL "AppleClang")
+if("${CMAKE_C_COMPILER_ID}" STREQUAL "Clang" OR "${CMAKE_C_COMPILER_ID}" STREQUAL "AppleClang" OR
+   "${CMAKE_C_COMPILER_ID}" STREQUAL "GNU")
+
   set(ZEEK_AGENT_COMMON_COMPILATION_FLAGS
     -Wall
     -Wextra
